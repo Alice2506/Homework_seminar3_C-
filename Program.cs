3ca3cc85b56661e02c3c1f665315e98f7eec3b5b
Alice2506 <ralko84@mail.ru>
2022-10-12 22:22:58 +0300

    task 1 added

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+
+int GetFirstSymbols(int num, int NumberOfFirstSymbols)
+{
+    while (num / Math.Pow(10, NumberOfFirstSymbols) > 1)
+    {
+        num/=10;
+    }
+    return num;
+}
+
+int number = int.Parse(Console.ReadLine());
+if (number < 0) number *= -1; 
+if (number > 9999 && number < 100000)
+{
+    int x = GetFirstSymbols(number,2); 
+    int y = number % 100;
+    if (x/10 == y%10 && y/10 == x%10)
+    {
+        Console.WriteLine("Введенное число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Введенное число не является палиндромом");
+    }
+} 
+else
+{
+    Console.WriteLine("Введенное число не является пятизначным");
+}
+
+
+
+
+    // if ((x-y) + x == y)
+    // {
+    //     Console.WriteLine("Введенное число является палиндромом");
+    // }
+    // else
+    // {
+    //     Console.WriteLine("Введенное число не является палиндромом");
+    // }
+
+
