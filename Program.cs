0b5d8898f9f03c52ec26caebb06d170537c45c8f
Alice2506 <ralko84@mail.ru>
2022-10-12 23:43:55 +0300

    added task 2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача 19
 //Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-
+/*
 Console.WriteLine("Введите пятизначное число: ");
 
 int GetFirstSymbols(int num, int NumberOfFirstSymbols)
@@ -32,4 +32,21 @@ else
     Console.WriteLine("Введенное число не является пятизначным");
 }
 
+*/
+void GetDistance()
+{
+    Console.WriteLine("Введите координаты первой точки: ");
+    int x1 = int.Parse(Console.ReadLine());
+    int y1 = int.Parse(Console.ReadLine());
+    int z1 = int.Parse(Console.ReadLine());
+    
+    Console.WriteLine("Введите координаты второй точки: ");
+    int x2 = int.Parse(Console.ReadLine());
+    int y2 = int.Parse(Console.ReadLine());
+    int z2 = int.Parse(Console.ReadLine());
+    double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+    
+    Console.WriteLine(distance);
+}    
+GetDistance();
 
