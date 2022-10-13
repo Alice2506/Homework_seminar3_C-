bb61ebc56a3e5d569cdf9ed63f76f3de34fc18a5
Alice2506 <ralko84@mail.ru>
2022-10-13 17:15:45 +0300

    added task 3

--- Program.cs
+++ Program.cs
@@ -54,3 +54,19 @@ void GetDistance()
 }    
 GetDistance();
 
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
+void GetCube()
+{
+    int N = int.Parse(Console.ReadLine());
+    for (int i = 1; i <= N; i++)
+    {
+        double result = Math.Pow(i, 3);
+        Console.WriteLine(result);
+    }
+}
+GetCube();
+
+
